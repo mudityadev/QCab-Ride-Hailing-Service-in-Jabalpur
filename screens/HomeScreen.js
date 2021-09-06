58a4845a6ea48786aafac03908659d44dbed40d9
Muditya Raghav <rmuditya@gmail.com>
2021-09-06 16:20:26 +0530

    navSlice created

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>I am homescreem</Text>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
